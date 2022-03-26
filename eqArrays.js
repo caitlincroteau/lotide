e82ee3bae803f48f88d5b3d949508f41b81316ce
Caitlin <cjcroteau@gmail.com>
2022-03-26 04:54:33 +0000

    updating eqArray to fix minor errors

--- eqArrays.js
+++ eqArrays.js
@@ -9,15 +9,15 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false
+    return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        return false
+        return false;
       }
     }
   }
-return true;
+  return true;
 };
 
 
