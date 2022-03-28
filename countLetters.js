74604fdd8b6382bbd170418927a1cca2290daa15
Caitlin <cjcroteau@gmail.com>
2022-03-28 18:50:48 +0000

    added loop to countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -8,7 +8,16 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(string) {
   const letters = {};
-
+  for (const letter of string) {
+    if (letter !== " ") {
+      if (letters[letter]) {
+        letters[letter] += 1;
+      } else {
+        letters[letter] = 1;
+      }
+    }
+  }
+  //console.log(letters);
   return letters;
 }
 
@@ -17,6 +26,10 @@ const string1 = "cait is so cool";
 const string2 = "little lily";
 const string3 = "ogopogo"
 
+
 assertEqual(countLetters(string1), {c: 2, a: 1, i: 2, t: 1, s: 2, o: 3, l: 1});
 assertEqual(countLetters(string2), {l: 4, i: 2, t: 2, e: 1, y: 1});
 assertEqual(countLetters(string3), {o: 4, g: 2, p: 1});
+
+
+//console.log(countLetters(string1));
