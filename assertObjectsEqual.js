175e62f9b4b273629b04331fd15aa60ace904483
Caitlin <cjcroteau@gmail.com>
2022-03-29 23:48:25 +0000

    changed variable names to actual and expected

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -38,18 +38,14 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const assertObjectsEqual = function(object1, object2) {
+const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  if (eqObjects(object1, object2)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`);
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
-
-
-
-
 
 const cd = {c: "1", d: ["2", 3]};
 const dc = {d: ["2", 3], c: "1"};
