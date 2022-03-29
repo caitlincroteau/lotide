489147ec8b6cf23317a0690fd5684189ba59c622
Caitlin <cjcroteau@gmail.com>
2022-03-29 22:36:25 +0000

    adding eqObjects file

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
 /*
 input: 2 objects
 returns true or false based on their match
@@ -13,13 +26,47 @@ returns true or false based on their match
 
 const eqObjects = function(object1, object2) {
 
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  //console.log(object1Keys);
+  //console.log(object2Keys);
+
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  } else {
+    for (let item of object1Keys) {
+      if (Array.isArray(object1[item]) && Array.isArray(object2[item])) {
+        let result = eqArrays(object1[item], object2[item]);
+        if (!result) {
+          return false;
+        }
+      } else if (object1[item] !== object2[item]) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
+
+//&& eqArrays(object1[item], object2[item]) === false
+
+const cd = {c: "1", d: ["2", 3]};
+const dc = {d: ["2", 3], c: "1"};
+assertEqual(eqObjects(cd, dc), true);
+//true
+
+const cd2 = {c: "1", d: ["2", 3, 4]};
+assertEqual(eqObjects(cd, cd2), false);
+//false
+
 
 const ab = {a: "1", b: "2"};
 const ba = {b: "2", a: "1"};
-eqObjects(ab, ba);
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(ab["a"], ba["a"]);
+assertEqual(ab["b"], ba["b"]);
 //true
 
 const abc = {a: "1", b: "2", c: "3"};
-eqObjects(ab, abc);
+assertEqual(eqObjects(ab, abc), false);
 //false
