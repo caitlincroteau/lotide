ffac75d4d1a24974e0f0143639502560da351885
Caitlin <cjcroteau@gmail.com>
2022-04-11 23:43:24 +0000

    updated assertArraysEqual.js and added new test file assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+//compares two arrays it takes in and prints out a message telling us if they match or not.
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
@@ -19,8 +10,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([], [1, 2]);
-assertArraysEqual(["hello", "hi"], ["hello", "hi"]);
-assertArraysEqual(["Caitlin", "Jean"], ["Seoul"]);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+console.log("this code will pass:");
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["hello", "hi"], ["hello", "hi"]);
+
+console.log("---");
+
+console.log("this code will fail:")
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([], [1, 2]);
+assertArraysEqual(["Caitlin", "Jean"], ["Seoul"]);
