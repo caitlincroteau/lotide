9a8e904c1a397917af10f26f8eae639dffa17da9
Caitlin <cjcroteau@gmail.com>
2022-04-15 21:35:43 +0000

    add comments to findKey file

--- findKey.js
+++ findKey.js
@@ -1,3 +1,6 @@
+//findKey function - scans an object and returns the first key for which the callback returns a truthy value.
+//If no key is found, returns undefined.
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -5,13 +8,6 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-/*
-Implement the function findKey which takes in an object and a callback.
-It should scan the object and return the first key for which the callback returns a truthy value.
-If no key is found, then it should return undefined.
-*/
-
 
 const findKey = function(object, callback) {
   const outerKeys = Object.keys(object);
